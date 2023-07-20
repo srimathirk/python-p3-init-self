893855b2d13016200e22b997f2fe600a6b551197
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-07-20 18:16:52 -0400

    init la done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self,name,breed = 'Mutt'):
+        self.name = name
+        self.breed = breed
+dog1 = Dog("chocky","pomerian")
+print(dog1.name)
+print(dog1.breed)
+
+dog2 = Dog("cookie","bulldog")
+print(dog2.name)
+print(dog2.breed)
+
+dog3 = Dog("Lucy")
+print(dog3.name)
+print(dog3.breed)
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self,name):
+        self.name = name
+
+person1 = Person("Ram")
+print(person1.name)
+person2 = Person("john")
+print(person2.name)
